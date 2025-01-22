8da2c69cc4d5dbc6f59b71f61e22e73c99ff077d
Felipe Carvalho <luizfelipecr401@gmail.com>
2025-01-21 23:45:44 -0300

    Game Repository, service, controller

---
+++ src/main/java/com/felipedev/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.felipedev.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.felipedev.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
