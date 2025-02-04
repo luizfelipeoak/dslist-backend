7ee0b1b7819ea88b0558123ee4f9cce98ba45cf0
Felipe Carvalho <luizfelipecr401@gmail.com>
2025-02-03 22:34:07 -0300

    [200~

--- src/main/java/com/felipedev/dslist/services/GameService.java
+++ src/main/java/com/felipedev/dslist/services/GameService.java
@@ -9,26 +9,34 @@ import org.springframework.transaction.annotation.Transactional;
 import com.felipedev.dslist.dto.GameDTO;
 import com.felipedev.dslist.dto.GameMinDTO;
 import com.felipedev.dslist.entities.Game;
+import com.felipedev.dslist.projections.GameMinProjection;
 import com.felipedev.dslist.repositories.GameRepository;
 
 @Service
 public class GameService {
-	
-	@Autowired GameRepository gameRepository;
-	
+
+	@Autowired
+	GameRepository gameRepository;
+
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
-		
+
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
-	
+
 	@Transactional(readOnly = true)
 	public GameDTO findById(Long id) {
-		
+
 		Game result = gameRepository.findById(id).get();
 		GameDTO dto = new GameDTO(result);
 		return dto;
+	}
+
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
